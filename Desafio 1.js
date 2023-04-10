9b1aed1a3d61b1654308cedaefec6859e981dc07
arkan0id3 <pruebafibermanu@gmail.com>
2023-04-10 09:59:56 -0300

    Add files via upload

--- Desafio 1.js
+++ Desafio 1.js
@@ -5,7 +5,7 @@ function createProduct(title, description, thumbnail, code, stock) {
         throw new Error('Todas las propiedades son necesarias');
     }
 
-    const obj = {
+    const prod = {
         id: ++id,
         title: title,
         description: description,
@@ -14,15 +14,15 @@ function createProduct(title, description, thumbnail, code, stock) {
         stock: stock,    
     };
     
-    return obj;
+    return prod;
 }
 
 // Ejemplo de uso:
-const myObject = createProduct('value1', 'value2', 'value3', 'value4', '22');
-console.log(myObject); // { id: 1, title: 'value1', description: 'value2', thumbnail: 'value3', code: 'value4', stock: '22' }
+const myProd = createProduct('value1', 'value2', 'value3', 'value4', '22');
+console.log(myProd); // { id: 1, title: 'value1', description: 'value2', thumbnail: 'value3', code: 'value4', stock: '22' }
 
 const anotherProduct = createProduct('foo', 'bar', 'baz', 'qux', '27');
-console.log(anotherObject); // { id: 2, title: 'foo', description: 'bar', thumbnail: 'baz', code: 'qux', stock: '27' }
+console.log(anotherProduct); // { id: 2, title: 'foo', description: 'bar', thumbnail: 'baz', code: 'qux', stock: '27' }
 
 // Error porque no tiene todos los valores
 createProduct('value1', 'value2', 'value3');
